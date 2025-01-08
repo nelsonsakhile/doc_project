3770f4822a9029a42a033dee13702623832a6bc9
nelson sakhile <nelsonsakhile@gmail.com>
2025-01-08 19:24:41 +0200

    Add client

---
+++ client/src/layouts/RootLayout.jsx
@@ -0,0 +1,26 @@
+import { Outlet } from "react-router-dom"
+import Navbar from "../components/navbar"
+import {Grid, GridItem} from "@chakra-ui/react"
+
+export default function RootLayout() {
+  return (
+    <Grid bg="gray.50" templateColumns = "repeat(6, 1fr)">
+      <GridItem
+        as="aside"
+        colSpan= "1"
+        bg = "green.300"
+        minHeight = "100vh"
+        p = "30px"
+      >
+        <span>side bar</span>
+      </GridItem>
+      <GridItem
+        as = "main"
+        colSpan= "5"
+      >
+      <Navbar />
+      <Outlet />
+      </GridItem>
+    </Grid>
+  )
+}
---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,22 @@
+import { SimpleGrid, Box} from "@chakra-ui/react"
+
+export default function Dashboard() {
+  return (
+    <SimpleGrid p="20px" minChildWidth="250px" spacing={10}>
+      <Box bg="white" h="200px" border="1px solid"> Som m</Box>
+      <Box bg="white" h="200px" border="1px solid"> Som m</Box>
+      <Box bg="white" h="200px" border="1px solid"> Som m</Box>
+      <Box bg="white" h="200px" border="1px solid"> Som m</Box>
+      
+      <Box bg="white" h="200px" border="1px solid"> Som m</Box>
+      <Box bg="white" h="200px" border="1px solid"> Som m</Box>
+      <Box bg="white" h="200px" border="1px solid"> Som m</Box>
+      <Box bg="white" h="200px" border="1px solid"> Som m</Box>
+      
+      <Box bg="white" h="200px" border="1px solid"> Som m</Box>
+      <Box bg="white" h="200px" border="1px solid"> Som m</Box>
+      <Box bg="white" h="200px" border="1px solid"> Som m</Box>
+      <Box bg="white" h="200px" border="1px solid"> Som m</Box>
+    </SimpleGrid>
+  )
+}
